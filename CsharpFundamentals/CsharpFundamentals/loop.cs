becb53701350ca16f04b781043814518355c34bc
Boktiar Mahamud <boktiarmahamud@gmail.com>
2026-05-20 00:48:10 +0600

    loop related example

---
+++ CsharpFundamentals/CsharpFundamentals/loop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFundamentals
+{
+    internal class loop
+    {
+        public static void Main(string[] args)
+        {
+            // For loop
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"For loop iteration: {i}");
+            }
+            // While loop
+            int j = 0;
+            while (j < 3)
+            {
+                Console.WriteLine($"While loop iteration: {j}");
+                j++;
+            }
+            // Do-while loop
+            int k = 0;
+            do
+            {
+                Console.WriteLine($"Do-while loop iteration: {k}");
+                k++;
+            } while (k < 3);
+        }
+    }
+}
