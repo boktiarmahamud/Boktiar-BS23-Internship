423332034fa722f3481bdebe5b238e43e139776e
Boktiar Mahamud <boktiarmahamud@gmail.com>
2026-05-20 00:42:21 +0600

    function related example

---
+++ CsharpFundamentals/CsharpFundamentals/function.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CsharpFundamentals
+{
+    internal class Function
+    {
+        public static void Main(string[] args)
+        {
+            int result = AddNumbers(10, 20, 30);
+
+            Console.WriteLine("Result: " + result);
+        }
+
+        static int AddNumbers(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+    }
+}
