7420149c38f98192687f7be9d1ec1d52ef2ac94a
Boktiar Mahamud <boktiarmahamud@gmail.com>
2026-05-20 01:52:36 +0600

    adding exception example

---
+++ CsharpFundamentals/CsharpFundamentals/ExceptionHandling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFundamentals
+{
+    internal class ExceptionHandling
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int result = a / b;
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed.");
+            }
+        }
+    }
+}
