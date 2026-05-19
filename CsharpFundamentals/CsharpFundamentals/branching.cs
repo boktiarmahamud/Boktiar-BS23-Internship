ce4265fb61d09bc2ec2986fd1fa1d71a33e0b9d7
Boktiar Mahamud <boktiarmahamud@gmail.com>
2026-05-20 00:35:09 +0600

    example about branching

---
+++ CsharpFundamentals/CsharpFundamentals/branching.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFundamentals
+{
+    internal class branching
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+        }
+    }
+}
