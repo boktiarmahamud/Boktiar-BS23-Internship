5a606f42fd92a20daeb02f60268bf82c35bba7c7
Boktiar Mahamud <boktiarmahamud@gmail.com>
2026-05-21 18:06:26 +0600

    Adding generic method and generic class examaple

---
+++ CsharpFundamentals/CsharpFundamentals/GenericClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpFundamentals
+{
+    // Base entity 
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+
+    // Product entity
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // Customer entity
+    public class Customer : BaseEntity
+    {
+        public string FullName { get; set; }
+    }
+
+    // Generic Repository
+    public class Repository<T> where T : BaseEntity
+    {
+        private readonly List<T> _data = new List<T>();
+
+        // CREATE
+        public void Add(T entity)
+        {
+            _data.Add(entity);
+        }
+
+        // READ (by Id)
+        public T GetById(int id)
+        {
+            return _data.FirstOrDefault(x => x.Id == id);
+        }
+
+        // READ ALL
+        public List<T> GetAll()
+        {
+            return _data;
+        }
+
+        // DELETE
+        public void Delete(int id)
+        {
+            var entity = GetById(id);
+            if (entity != null)
+            {
+                _data.Remove(entity);
+            }
+        }
+
+        // UPDATE
+        public void Update(T entity)
+        {
+            var existing = GetById(entity.Id);
+            if (existing != null)
+            {
+                int index = _data.IndexOf(existing);
+                _data[index] = entity;
+            }
+        }
+    }
+
+    internal class GenericClass
+    {
+        public static void Main(string[] args)
+        {
+            // Repository for Product
+            var productRepo = new Repository<Product>();
+
+            productRepo.Add(new Product { Id = 1, Name = "Laptop", Price = 80000 });
+            productRepo.Add(new Product { Id = 2, Name = "Mouse", Price = 200 });
+
+            var product = productRepo.GetById(1);
+
+            Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+
+            // Repository for Customer
+            var customerRepo = new Repository<Customer>();
+
+            customerRepo.Add(new Customer { Id = 6, FullName = "Boktiar" });
+
+            var customer = customerRepo.GetById(1);
+
+            Console.WriteLine($"Customer: {customer.FullName}");
+        }
+    }
+}
---
+++ CsharpFundamentals/CsharpFundamentals/GenericMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFundamentals
+{
+    public class Generic
+    {
+        // Generic method to display elements of an array
+        public static void ShowArray<T>(T[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+    internal class GenericMethods
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            string[] stringArray = { "Hello", "World", "Generic", "Methods" };
+            Console.WriteLine("Integer Array:");
+            Generic.ShowArray(intArray);
+            Console.WriteLine("\nString Array:");
+            Generic.ShowArray(stringArray);
+        }
+        
+    }
+}
